19bcc098722293c1dedb770066532f7429ef1f47
ANARX <ANARX@users.noreply.github.com>
2022-04-15 19:49:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Employees app!
+
+### Tech stack
+- _React_
+
+### How to use
+_1. Clone/Download the repo._
+
+_2. Run  ``` npm i ```._
+
+_3. Run ```npm start```_
---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+  const data = [
+    { name: 'John K.', salary: 5000 },
+    { name: 'Cecile H.', salary: 3000 },
+    { name: 'Bart N.', salary: 4500 },
+  ];
+
+  return (
+    <div className="app">
+      <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
+    </div>
+  );
+}
+
+export default App;
